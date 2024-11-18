03da12c766bef3ad940231724e17300e762037e0
leochris7 <leo.christophe@etu.univ-smb.fr>
2024-11-18 09:31:50 +0100

    Doc

---
+++ API_OVH/Controllers/CapteursController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API_OVH.Models.EntityFramework;
+
+namespace API_OVH.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CapteursController : ControllerBase
+    {
+        private readonly SAE5_BD_OVH_DbContext _context;
+
+        public CapteursController(SAE5_BD_OVH_DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Capteurs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Capteur>>> GetCapteurs()
+        {
+            return await _context.Capteurs.ToListAsync();
+        }
+
+        // GET: api/Capteurs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Capteur>> GetCapteur(int id)
+        {
+            var capteur = await _context.Capteurs.FindAsync(id);
+
+            if (capteur == null)
+            {
+                return NotFound();
+            }
+
+            return capteur;
+        }
+
+        // PUT: api/Capteurs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCapteur(int id, Capteur capteur)
+        {
+            if (id != capteur.IdCapteur)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(capteur).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CapteurExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Capteurs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Capteur>> PostCapteur(Capteur capteur)
+        {
+            _context.Capteurs.Add(capteur);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCapteur", new { id = capteur.IdCapteur }, capteur);
+        }
+
+        // DELETE: api/Capteurs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCapteur(int id)
+        {
+            var capteur = await _context.Capteurs.FindAsync(id);
+            if (capteur == null)
+            {
+                return NotFound();
+            }
+
+            _context.Capteurs.Remove(capteur);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CapteurExists(int id)
+        {
+            return _context.Capteurs.Any(e => e.IdCapteur == id);
+        }
+    }
+}
---
+++ API_OVH/Models/Manager/BatimentManager.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GestionProduit_API.Models.Repository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API_OVH.Models.EntityFramework;
+using AutoMapper;
+
+namespace API_OVH.Models.Manager
+{
+    /// <summary>
+    /// Manager pour gérer les opérations liées aux produits, en utilisant les DTOs pour la manipulation des données.
+    /// </summary>
+    public class BatimentManager : IDataRepository<Batiment>
+    {
+        private readonly SAE5_BD_OVH_DbContext _context;
+        private readonly IMapper _mapper;
+
+        [ActivatorUtilitiesConstructor]
+        public BatimentManager(SAE5_BD_OVH_DbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public BatimentManager()
+        {
+        }
+
+        /// <summary>
+        /// Retourne la liste des batiments de façon asynchrone
+        /// </summary>
+        /// <returns>La liste des batiments</returns>
+        public async Task<ActionResult<IEnumerable<Batiment>>> GetAllAsync()
+        {
+            var batiments = await _context.Batiments
+                            .ToListAsync();
+
+            return batiments;
+        }
+
+        /// <summary>
+        /// Retourne un batiment selon son id de façon asynchrone
+        /// </summary>
+        /// <param name="id">(Entier) Identifiant du batiment</param>
+        /// <returns>Le batiment correspondant à l'ID</returns>
+        public async Task<ActionResult<Batiment>> GetByIdAsync(int id)
+        {
+            var leBatiment = await _context.Batiments
+                             .FirstOrDefaultAsync(x => x.Idbatiment == id);
+
+            // S'il n'est pas trouvé
+            if (leBatiment == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return leBatiment;
+        }
+
+        /// <summary>
+        /// Retourne un batiment selon son nom de façon asynchrone
+        /// </summary>
+        /// <param name="str">Nom du batiment</param>
+        /// <returns>Le batiment correspondant au nom spécifié</returns>
+        public async Task<ActionResult<Batiment>> GetByStringAsync(string str)
+        {
+            var leBatiment = await _context.Batiments
+                             .FirstOrDefaultAsync(x => x.Nombatiment == str);
+
+            // Si non trouvé
+            if (leBatiment == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return leBatiment;
+        }
+
+        /// <summary>
+        /// Ajoute un batiment de façon asynchrone
+        /// </summary>
+        /// <param name="entity">Batiment à rajouter</param>
+        /// <returns>Le batiment créée</returns>
+        public async Task AddAsync(Batiment entity)
+        {
+            await _context.Batiments.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Met à jour un batiment de façon asynchrone
+        /// </summary>
+        /// <param name="entityToUpdate">Batiment à mettre à jour</param>
+        /// <param name="entity">Batiment avec les valeurs mis à jour</param>
+        /// <returns>Résultat de l'opération</returns>
+        public async Task UpdateAsync(Batiment entityToUpdate, Batiment entity)
+        {
+            _context.Entry(entityToUpdate).State = EntityState.Modified;
+
+            entityToUpdate.Nombatiment = entity.Nombatiment;
+
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Permet de supprimer un batiment de façon asynchrone
+        /// </summary>
+        /// <param name="entity">Le batiment à supprimer</param>
+        /// <returns>Le résultat de l'opération</returns>
+        public async Task DeleteAsync(Batiment entity)
+        {
+            _context.Batiments.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
