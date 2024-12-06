c73d309e8cd896980dc46e49a4e4f048d440633e
Dorian GEHANT <gehantd@iut-acy.local>
2024-12-06 10:52:56 +0100

    Avancement automapper pour batiment et capteur

--- API_OVH/Models/DTO/AutoMapperProfile.cs
+++ API_OVH/Models/DTO/AutoMapperProfile.cs
@@ -8,6 +8,72 @@ namespace API_OVH
     {
         public AutoMapperProfile()
         {
+            // Batiment
+            CreateMap<Batiment, BatimentDTO>()
+                .ForMember(dest => dest.NomBatiment, opt => opt.MapFrom(src => src.NomBatiment))
+                .ForMember(dest => dest.IdBatiment, opt => opt.MapFrom(src => src.IdBatiment))
+                .ForMember(dest => dest.NbSalle, opt => opt.MapFrom(src => src.Salles.Count))
+                .ReverseMap()
+                .ForMember(dest => dest.NomBatiment, opt => opt.MapFrom(src => src.NomBatiment))
+                .ForMember(dest => dest.IdBatiment, opt => opt.MapFrom(src => src.IdBatiment))
+                .ForMember(dest => dest.Salles, opt => opt.Ignore());
+
+            CreateMap<Batiment, BatimentSansNavigationDTO>()
+                .ForMember(dest => dest.NomBatiment, opt => opt.MapFrom(src => src.NomBatiment))
+                .ForMember(dest => dest.IdBatiment, opt => opt.MapFrom(src => src.IdBatiment))
+                .ReverseMap()
+                .ForMember(dest => dest.NomBatiment, opt => opt.MapFrom(src => src.NomBatiment))
+                .ForMember(dest => dest.IdBatiment, opt => opt.MapFrom(src => src.IdBatiment));
+
+            // Capteur
+            CreateMap<Capteur, CapteurDetailDTO>()
+                .ForMember(dest => dest.IdCapteur, opt => opt.MapFrom(src => src.IdCapteur))
+                .ForMember(dest => dest.NomCapteur, opt => opt.MapFrom(src => src.NomCapteur))
+                .ForMember(dest => dest.EstActif, opt => opt.MapFrom(src => src.EstActif))
+                .ForMember(dest => dest.XCapteur, opt => opt.MapFrom(src => src.XCapteur))
+                .ForMember(dest => dest.YCapteur, opt => opt.MapFrom(src => src.YCapteur))
+                .ForMember(dest => dest.ZCapteur, opt => opt.MapFrom(src => src.ZCapteur))
+                .ForMember(dest => dest.Mur, opt => opt.MapFrom(src => src.MurNavigation))
+                .ForMember(dest => dest.Salle, opt => opt.MapFrom(src => src.MurNavigation.SalleNavigation))
+                .ForMember(dest => dest.Unites, opt => opt.MapFrom(src => src.UnitesCapteur.Select(uc => uc.UniteNavigation)))
+                .ReverseMap()
+                .ForMember(dest => dest.IdCapteur, opt => opt.MapFrom(src => src.IdCapteur))
+                .ForMember(dest => dest.NomCapteur, opt => opt.MapFrom(src => src.NomCapteur))
+                .ForMember(dest => dest.EstActif, opt => opt.MapFrom(src => src.EstActif))
+                .ForMember(dest => dest.XCapteur, opt => opt.MapFrom(src => src.XCapteur))
+                .ForMember(dest => dest.YCapteur, opt => opt.MapFrom(src => src.YCapteur))
+                .ForMember(dest => dest.ZCapteur, opt => opt.MapFrom(src => src.ZCapteur))
+                .ForMember(dest => dest.MurNavigation, opt => opt.MapFrom(src => src.Mur))
+                .ForMember(dest => dest.UnitesCapteur, opt => opt.MapFrom(src => src.Unites.Select(unite => new UniteCapteur // Pour chaque Unite on "recrée" l'UniteCapteur correspondant en prenant l'id de l'Unite avec l'id du Capteur
+                {
+                    IdUnite = unite.IdUnite,
+                    IdCapteur = src.IdCapteur
+                })));
+
+            CreateMap<Capteur, CapteurSansNavigationDTO>()
+                .ForMember(dest => dest.IdCapteur, opt => opt.MapFrom(src => src.IdCapteur))
+                .ForMember(dest => dest.IdMur, opt => opt.MapFrom(src => src.IdMur))
+                .ForMember(dest => dest.NomCapteur, opt => opt.MapFrom(src => src.NomCapteur))
+                .ForMember(dest => dest.EstActif, opt => opt.MapFrom(src => src.EstActif))
+                .ForMember(dest => dest.XCapteur, opt => opt.MapFrom(src => src.XCapteur))
+                .ForMember(dest => dest.YCapteur, opt => opt.MapFrom(src => src.YCapteur))
+                .ForMember(dest => dest.ZCapteur, opt => opt.MapFrom(src => src.ZCapteur))
+                .ReverseMap()
+                .ForMember(dest => dest.IdCapteur, opt => opt.MapFrom(src => src.IdCapteur))
+                .ForMember(dest => dest.IdMur, opt => opt.MapFrom(src => src.IdMur))
+                .ForMember(dest => dest.NomCapteur, opt => opt.MapFrom(src => src.NomCapteur))
+                .ForMember(dest => dest.EstActif, opt => opt.MapFrom(src => src.EstActif))
+                .ForMember(dest => dest.XCapteur, opt => opt.MapFrom(src => src.XCapteur))
+                .ForMember(dest => dest.YCapteur, opt => opt.MapFrom(src => src.YCapteur))
+                .ForMember(dest => dest.ZCapteur, opt => opt.MapFrom(src => src.ZCapteur))
+                .ForMember(dest => dest.MurNavigation, opt => opt.Ignore())
+                .ForMember(dest => dest.UnitesCapteur, opt => opt.Ignore());
+
+            CreateMap<Capteur, CapteurDTO>()
+                .ForMember(dest => dest.IdCapteur, opt => opt.MapFrom(src => src.IdCapteur))
+                .ForMember(dest => dest.NomCapteur, opt => opt.MapFrom(src => src.NomCapteur))
+                .ForMember(dest => dest.NomSalle, opt => opt.MapFrom(src => src.MurNavigation.SalleNavigation.NomSalle));
+
             // TypeEquipement
             CreateMap<TypeEquipement, TypeEquipementDTO>()
                 .ForMember(dest => dest.NomTypeEquipement, opt => opt.MapFrom(src => src.NomTypeEquipement))
--- API_OVH/Models/DTO/CapteurDetailDTO.cs
+++ API_OVH/Models/DTO/CapteurDetailDTO.cs
@@ -1,4 +1,5 @@
 ﻿using API_OVH.Models.EntityFramework;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API_OVH.Models.DTO
 {
@@ -7,13 +8,23 @@ namespace API_OVH.Models.DTO
         private int idCapteur;
         private string nomCapteur;
         private string nomSalle;
+        private string estActif;
+        private decimal xCapteur;
+        private decimal yCapteur;
+        private decimal zCapteur;
         private List<Unite> unites;
         private Mur mur;
+        private Salle salle;
 
+        public int IdCapteur { get => idCapteur; set => idCapteur = value; }
         public string NomCapteur { get => nomCapteur; set => nomCapteur = value; }
         public string NomSalle { get => nomSalle; set => nomSalle = value; }
+        public string EstActif { get => estActif; set => estActif = value; }
+        public decimal XCapteur { get => xCapteur; set => xCapteur = value; }
+        public decimal YCapteur { get => yCapteur; set => yCapteur = value; }
+        public decimal ZCapteur { get => zCapteur; set => zCapteur = value; }
         public List<Unite> Unites { get => unites; set => unites = value; }
         public Mur Mur { get => mur; set => mur = value; }
-        public int IdCapteur { get => idCapteur; set => idCapteur = value; }
+        public Salle Salle { get => salle; set => salle = value; }
     }
 }
